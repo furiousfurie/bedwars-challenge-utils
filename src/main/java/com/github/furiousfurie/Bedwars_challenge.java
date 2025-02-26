e02af460202cac869e14427c3c7c1004eaddf79c
furiousfurie <135446309+furiousfurie@users.noreply.github.com>
2025-02-26 16:30:17 +0100

    Update Bedwars_challenge.java

--- src/main/java/com/github/furiousfurie/Bedwars_challenge.java
+++ src/main/java/com/github/furiousfurie/Bedwars_challenge.java
@@ -18,6 +18,11 @@ public class Bedwars_challenge {
     public static final String MODID = "bedwars_challenge_utils";
     public static final String VERSION = "1.0";
     public static final String MODNAME = "Bedwars challenge Utils";
+    //totally my code that i didn't steal
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        ConfigHandler.loadConfig(event.getSuggestedConfigurationFile());
+    }
 
     @EventHandler
     public void init(FMLInitializationEvent event){
