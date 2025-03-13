26ed915a7807663026273d67a2d084121a1abb60
amolinarius <89464182+amolinarius@users.noreply.github.com>
2025-03-13 18:57:34 +0100

    Enhance itemlist readability

--- src/main/java/com/github/furiousfurie/InvisibleShopper.java
+++ src/main/java/com/github/furiousfurie/InvisibleShopper.java
@@ -5,6 +5,7 @@ import net.minecraft.util.ChatComponentText;
 import net.minecraftforge.event.ServerChatEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -13,13 +14,34 @@ import java.util.Scanner;
 public class InvisibleShopper {
     EntityPlayer player = Minecraft.getMinecraft().thePlayer;;
     Map<Integer, String> iron_item_list = new HashMap<Integer, String>() {{
-        put(4,"wool");
-        put(12, "clay/glass");
-        put(24, "endstone/chain armor/bedbug/pop-up_tower\"");
-        put(20, "shears");
-        put(10, "axe/pickaxe/stone sword");
-        put(120, "dream_defenders");
-        put(40, "fireball");
+        put(4, new ArrayList<String>() {{
+            add("WOOL");
+            add("LADDER");
+        }});
+        put(10,  new ArrayList<String>() {{
+            add("STONE_SWORD");
+            add("WOODEN_PICKAXE");
+            add("WOODEN_AXE");
+        }});
+        put(12, new ArrayList<String>() {{
+            add("HARDENED_CLAY");
+            add("GLASS");
+        }});
+        put(20,  new ArrayList<String>() {{
+            add("SHEARS");
+        }});
+        put(24,  new ArrayList<String>() {{
+            add("ENDSTONE");
+            add("CHAINMAIL_ARMOR");
+            add("BEDBUG");
+            add("POPUP_TOWER");
+        }});
+        put(40,  new ArrayList<String>() {{
+            add("FIREBALL");
+        }});
+        put(120, new ArrayList<String>() {{
+            add("DREAM_DEFENDER");
+        }});
     }};
 
     @SubscribeEvent()
