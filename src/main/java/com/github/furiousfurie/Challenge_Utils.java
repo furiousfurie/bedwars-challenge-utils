55ae3f6c33c9b0ccddd1ac3d1a8f7bac341e1e00
amolinarius <89464182+amolinarius@users.noreply.github.com>
2025-03-14 17:57:30 +0100

    Edit main file

--- src/main/java/com/github/furiousfurie/Challenge_Utils.java
+++ src/main/java/com/github/furiousfurie/Challenge_Utils.java
@@ -13,16 +13,16 @@ import java.util.Map;
 import java.util.regex.Pattern;
 
 
-@Mod(modid = Bedwars_challenge.MODID, version = Bedwars_challenge.VERSION, modLanguage = "java")
-public class Bedwars_challenge {
+@Mod(modid = Challenge_Utils.MODID, version = Challenge_Utils.VERSION, modLanguage = "java")
+public class Challenge_Utils {
     public static final String MODID = "bedwars_challenge_utils";
     public static final String VERSION = "1.0";
-    public static final String MODNAME = "Bedwars challenge Utils";
+    public static final String MODNAME = "Bedwars Challenge Utils";
 
     @EventHandler
     public void init(FMLInitializationEvent event){
         MinecraftForge.EVENT_BUS.register(new InvisibleShopper());
-        System.out.println("Bedwars challenge utils initialization begun");
+        System.out.println("Initializing "+Challenge_Utils.MODNAME+" v"+Challenge_Utils.VERSION);
     }
 }
 
