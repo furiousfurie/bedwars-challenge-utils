140e352adaa0bea749af2661ef8bb26db79f6749
amolinarius <89464182+amolinarius@users.noreply.github.com>
2025-03-16 12:12:46 +0100

    Add basic slot click detection

--- src/main/java/com/github/furiousfurie/InvisibleShopper.java
+++ src/main/java/com/github/furiousfurie/InvisibleShopper.java
@@ -1,14 +1,23 @@
 package com.github.furiousfurie;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.inventory.GuiChest;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.Container;
+import net.minecraft.inventory.ContainerChest;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ChatComponentText;
+import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.event.ServerChatEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+
+import org.lwjgl.input.Mouse;
 
 
 public class InvisibleShopper {
@@ -44,8 +53,8 @@ public class InvisibleShopper {
         }});
     }};
 
-    @SubscribeEvent()
-    public void chat_received_event(ServerChatEvent event){
+    @SubscribeEvent
+    public void chat_received_event(ServerChatEvent event) {
         Scanner scanner = new Scanner(System.in);
         String msgNumbers = event.message.replaceAll("[^0-9]", ""); // Use regex to remove all non-numeric characters
         scanner.close();
@@ -55,6 +64,27 @@ public class InvisibleShopper {
         if(iron_items.containsKey(price)){
             Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText(String.join(", ", iron_items.get(price))));
         }
+    }
+
+    @SubscribeEvent
+    public void item_clicked_event(GuiScreenEvent.MouseInputEvent.Post event) {
+        if (!(event.gui instanceof GuiChest)) {return;}
+
+        GuiChest guiChest = (GuiChest) event.gui;
+        Container chest = guiChest.inventorySlots;
+        //TODO: galaxymiller: Add other languages support
+        List<String> shopkeeperGuis = Arrays.asList("Quick Buy", "Blocks", "Melee", "Armor", "Tools", "Ranged", "Potions", "Utility", "Rotating Items");
+
+        if (!(chest instanceof ContainerChest)) {return;}
+        String guiName = ((ContainerChest) chest).getLowerChestInventory().getName();
+        if (!shopkeeperGuis.stream().anyMatch(x -> x.equalsIgnoreCase(guiName))) {return;} //? https://stackoverflow.com/a/15269846
+        if (!Mouse.isButtonDown(0)) {return;}
+
+        ItemStack heldItem = Minecraft.getMinecraft().thePlayer.inventory.getItemStack();
+        if (heldItem == null) {return;}
+
+        System.out.println("Clicked item" + heldItem.getDisplayName());
+        //TODO: furiousfurie: Add chat reading system to get item price
     }
 }
 
